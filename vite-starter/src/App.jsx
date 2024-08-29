3af7d8a0ccfffa55873752e9220807f252ca4584
gyojinnK <ksmug1998@gmail.com>
2024-08-29 18:28:56 +0900

    feat: 간단한 앱으로 테스트 실습

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import { kebabCaseToTitleCase } from "./helpers";
+
 function App() {
+  const [buttonColor, setButtonColor] = useState("medium-violet-red");
+  const nextColorClass =
+    buttonColor === "medium-violet-red" ? "midnight-blue" : "medium-violet-red";
+  const nextColorTitleCase = kebabCaseToTitleCase(nextColorClass);
+  const [isDisable, setIsDisable] = useState(false);
+  const checkboxClickHandler = () => {
+    setIsDisable(!isDisable);
+  };
+
   return (
     <div>
-      <h1>I'm gonna learn React Testing Library</h1>
+      <button
+        className={isDisable ? "gray" : buttonColor}
+        onClick={() => setButtonColor(nextColorClass)}
+        disabled={isDisable}
+      >
+        Change to {nextColorTitleCase}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={false}
+        onClick={checkboxClickHandler}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,92 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { kebabCaseToTitleCase } from "./helpers";
 
-test("App contains correct heading", () => {
+test("버튼 클릭 flow", () => {
+  // render App
   render(<App />);
-  const headingElement = screen.getByRole("heading", { name: /learn React/i });
-  expect(headingElement).toBeInTheDocument();
+
+  // find the button
+  const btnElement = screen.getByRole("button", { name: /Midnight Blue/i });
+
+  // check initial color
+  expect(btnElement).toHaveClass("medium-violet-red");
+
+  // click the button
+  fireEvent.click(btnElement);
+
+  // check button text
+  expect(btnElement).toHaveTextContent(/medium violet red/i);
+
+  // check the button color
+  expect(btnElement).toHaveClass("midnight-blue");
+});
+
+test("체크박스 flow", () => {
+  render(<App />);
+  const btnElement = screen.getByRole("button", { name: /midnight blue/i });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+  expect(btnElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+
+  fireEvent.click(checkboxElement);
+
+  expect(btnElement).not.toBeEnabled();
+  expect(checkboxElement).toBeChecked();
+
+  fireEvent.click(checkboxElement);
+
+  expect(btnElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+});
+
+test("체크박스 disabled flow", () => {
+  render(<App />);
+  const btnElement = screen.getByRole("button");
+  const chkboxElement = screen.getByRole("checkbox");
+
+  // 초기 버튼, 체크박스 상태 테스트
+  expect(btnElement).toBeEnabled();
+  expect(chkboxElement).not.toBeChecked();
+
+  // 초기 상태에서 disabled 테스트
+  fireEvent.click(chkboxElement);
+  expect(btnElement).toHaveClass("gray");
+  expect(btnElement).not.toBeEnabled();
+  expect(chkboxElement).toBeChecked();
+
+  fireEvent.click(chkboxElement);
+  expect(btnElement).toHaveClass("medium-violet-red");
+  expect(btnElement).toBeEnabled();
+  expect(chkboxElement).not.toBeChecked();
+
+  // 버튼 클릭 시 disabled 테스트
+  fireEvent.click(btnElement);
+  expect(btnElement).toHaveClass("midnight-blue");
+
+  fireEvent.click(chkboxElement);
+  expect(chkboxElement).toBeChecked();
+  expect(btnElement).not.toBeEnabled();
+  expect(btnElement).toHaveClass("gray");
+
+  fireEvent.click(chkboxElement);
+  expect(chkboxElement).not.toBeChecked();
+  expect(btnElement).toBeEnabled();
+  expect(btnElement).toHaveClass("midnight-blue");
+});
+
+// 테스트를 그룹화
+// 함수 단위의 유닛 테스트
+describe("kebabCaseToTitleCase", () => {
+  test("하이픈 없음 테스트", () => {
+    expect(kebabCaseToTitleCase("red")).toBe("Red");
+  });
+  test("하이픈 1개 테스트", () => {
+    expect(kebabCaseToTitleCase("midnight-blue")).toBe("Midnight Blue");
+  });
+  test("하이픈 n개 테스트", () => {
+    expect(kebabCaseToTitleCase("medium-violet-red")).toBe("Medium Violet Red");
+  });
 });
