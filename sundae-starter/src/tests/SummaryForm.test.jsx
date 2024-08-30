53977fba1d3d2784475dd7906fb328c5255b5115
gyojinnK <ksmug1998@gmail.com>
2024-08-30 20:38:21 +0900

    feat: userEvent 추가, 비동기 테스트

--- sundae-starter/src/tests/SummaryForm.test.jsx
+++ sundae-starter/src/tests/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../summary/SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("초기 상태 테스트", () => {
   render(<SummaryForm />);
@@ -10,16 +11,39 @@ test("초기 상태 테스트", () => {
   expect(confirmBtn).toBeDisabled();
 });
 
-test("체크박스를 클릭(체크) -> 확인 버튼 활성화", () => {
+test("체크박스를 클릭(체크) -> 확인 버튼 활성화", async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
   const chkbox = screen.getByRole("checkbox", { name: /i agree to/i });
   const confirmBtn = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(chkbox);
+  await user.click(chkbox);
   expect(chkbox).toBeChecked();
   expect(confirmBtn).toBeEnabled();
 
-  fireEvent.click(chkbox);
+  await user.click(chkbox);
   expect(chkbox).not.toBeChecked();
   expect(confirmBtn).toBeDisabled();
+});
+
+test("hover 시 popover가 등장한다.", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // 초기 상태: hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // hover했을 경우: visible
+  const termsAndCondition = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndCondition);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // unHover했을 경우: hidden
+  await user.unhover(termsAndCondition);
+  expect(popover).not.toBeInTheDocument();
 });
